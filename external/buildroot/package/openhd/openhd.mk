453405a73836806bf6301e6a95f0071cb06a44ad
Raphael <scholle@uni-wuppertal.de>
2024-09-28 18:52:56 +0200

    Update openhd.mk

--- external/buildroot/package/openhd/openhd.mk
+++ external/buildroot/package/openhd/openhd.mk
@@ -3,15 +3,30 @@
 # OpenHD
 #
 ################################################################################
-OPENHD_SITE = https://github.com/OpenHD/OpenHD
+
+# The Git repository from which to clone the source code
+OPENHD_SITE = https://github.com/OpenHD/OpenHD.git
 OPENHD_SITE_METHOD = git
 OPENHD_GIT_SUBMODULES = YES
+
+# The specific commit or tag to checkout
 OPENHD_VERSION = fbb6ed1ffe00b66e1d32fd9c9b5830b84d3a9c4b
+
+# Enable Git submodules if the project requires them
+OPENHD_GIT_SUBMODULES = YES
+
+# Subdirectory inside the Git repo, if needed (if OpenHD is not in the root)
 OPENHD_SUBDIR = OpenHD
 
+# No need to install to the staging directory, only target
 OPENHD_INSTALL_STAGING = NO
 OPENHD_INSTALL_TARGET = YES
 
+# Additional configuration options for the CMake build
 OPENHD_CONF_OPTS = -DENABLE_USB_CAMERAS=OFF
+
+# List of dependencies that must be built before OpenHD
 OPENHD_DEPENDENCIES = libsodium gstreamer1 gst1-plugins-base libpcap host-pkgconf
+
+# Use Buildroot's CMake package infrastructure to handle the build
 $(eval $(cmake-package))
