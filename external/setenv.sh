83d9badecc016fa28e1fb32e59492d042e3e67ec
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-10-11 00:12:49 +0200

    update poco

--- external/setenv.sh
+++ external/setenv.sh
@@ -46,8 +46,9 @@ echo "patching openhd into config.in"
 sed -i '/menu "Audio and video applications"/a\        source "package/openhd/Config.in"' "$BUILDROOT_DIR/package/Config.in"
 echo "patching poco"
 sed -i 's/c++14/c++17/g' buildroot-2021.05/package/poco/poco.mk
+rm -Rf buildroot-2021.05/package/poco/poco.hash
 sed -i 's|POCO_SITE = $(call github,pocoproject,poco,poco-$(POCO_VERSION)-release)|POCO_SITE = $(call github,OpenHD,poco,v$(POCO_VERSION))|' buildroot-2021.05/package/poco/poco.mk
-sed -i 's/1.10.1/main/g' buildroot-2021.05/package/poco/poco.mk
+sed -i 's/1.10.1/1.13.3/g' buildroot-2021.05/package/poco/poco.mk
 rsync_dir $EXTERNAL/isp_tuning .
 rsync_dir $EXTERNAL/ramdisk/ ramdisk/
 rsync_dir $EXTERNAL/u-boot/ $UBOOT_DIR/
