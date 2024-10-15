b8dbbc4c4109f1a71769b4d60b945cda4363c03c
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-10-16 01:55:57 +0200

    include openhd

--- external/setenv.sh
+++ external/setenv.sh
@@ -11,6 +11,14 @@ function rsync_dir()
 }
 
 ###################################
+# updating packages before sync
+###################################
+echo "updating poco"
+rm -Rf buildroot-2021.05/package/poco/*
+cp -v external/updates/* buildroot-2021.05/package/poco/
+
+
+###################################
 # rsync codes
 ###################################
 mkdir -p $PROJECT_OUT
@@ -42,6 +50,8 @@ rsync_dir ./cvi_rtsp
 ###################################
 rsync_dir $EXTERNAL/build .
 rsync_dir $EXTERNAL/buildroot/ $BUILDROOT_DIR/
+echo "patching openhd into config.in"
+sed -i '/menu "Audio and video applications"/a\        source "package/openhd/Config.in"' "$BUILDROOT_DIR/package/Config.in"
 rsync_dir $EXTERNAL/isp_tuning .
 rsync_dir $EXTERNAL/ramdisk/ ramdisk/
 rsync_dir $EXTERNAL/u-boot/ $UBOOT_DIR/
