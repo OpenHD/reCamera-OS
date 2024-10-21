d9cb2398241e2487a5150071e2295160d83fe425
Raphael <scholle@uni-wuppertal.de>
2024-10-21 22:47:59 +0200

    Update setenv.sh

--- external/setenv.sh
+++ external/setenv.sh
@@ -55,7 +55,7 @@ rsync_dir $EXTERNAL/build .
 rsync_dir $EXTERNAL/buildroot/ $BUILDROOT_DIR/
 echo "patching openhd into config.in"
 sed -i '/menu "Audio and video applications"/a\        source "package/openhd/Config.in"' "$BUILDROOT_DIR/package/Config.in"
-sed -i '/menu "Audio and video applications"/a\        source "package/rtl8812eu-openhd/Config.in"' "$BUILDROOT_DIR/package/Config.in"
+sed -i '/menu "Networking"/a\        source "package/rtl8812eu-openhd/Config.in"' "$BUILDROOT_DIR/package/Config.in"
 rsync_dir $EXTERNAL/isp_tuning .
 rsync_dir $EXTERNAL/ramdisk/ ramdisk/
 rsync_dir $EXTERNAL/u-boot/ $UBOOT_DIR/
